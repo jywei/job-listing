dbe8c5ed2040761543606cb57e744ee052ded490
Shane <shane.zhou@sarbeer.com>
2016-07-27 21:36:44 +0800

    post job complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root 'pages#index'
 
+  get 'pages/index'
   resources :jobs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
