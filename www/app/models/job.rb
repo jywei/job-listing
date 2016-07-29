4ea1e440851bf5c3ba706e7e81c2554713300ef3
Roy Wei <roywjy@gmail.com>
2016-07-29 17:43:44 +0800

    add filter function

--- www/app/models/job.rb
+++ www/app/models/job.rb
@@ -11,4 +11,91 @@ class Job < ActiveRecord::Base
     all
   end
 
+  filterrific(
+    default_filter_params: { sorted_by: 'created_at_desc' },
+    available_filters: [
+      :sorted_by,
+      :search_query,
+      :with_category_id,
+      :with_industry_id,
+      :with_created_at_gte
+    ]
+  )
+
+  # default for will_paginate
+  self.per_page = 10
+
+  scope :search_query, -> (query) {
+    return nil if query.blank?
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conditions = 3
+    where(
+      terms.map {
+        or_clauses = [
+          "LOWER(jobs.title) LIKE ?",
+          "LOWER(jobs.description) LIKE ?",
+          "LOWER(jobs.requirement) LIKE ?"
+        ].join(' OR ')
+        "(#{ or_clauses })"
+      }.join(' AND '),
+      *terms.map { |e| [e] * num_or_conditions }.flatten
+    )
+  }
+
+  scope :sorted_by, -> (sort_option) {
+    # extract the sort direction from the param value.
+    direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+    case sort_option.to_s
+    when /^created_at_/
+      order("jobs.created_at #{ direction }")
+    when /^title_/
+      order("LOWER(jobs.title) #{ direction }")
+    when /^industry_name_/
+      order("LOWER(industries.name) #{ direction }").includes(:industry)
+    when /^category_name_/
+      order("LOWER(categories.name) #{ direction }").includes(:category)
+    else
+      raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+    end
+  }
+
+  scope :with_industry_id, -> (industry_ids) {
+    where(industry_id: [*industry_ids])
+  }
+
+  scope :with_category_id, -> (category_ids) {
+    where(category_id: [*category_ids])
+  }
+
+  scope :with_created_at_gte, -> (ref_date) {
+    where('jobs.created_at >= ?', ref_date)
+  }
+
+  delegate :name, to: :industry, prefix: true
+
+  delegate :name, to: :category, prefix: true
+
+  def self.options_for_sorted_by
+    [
+      ['Title (a-z)', 'title_asc'],
+      ['Creation date (newest first)', 'created_at_desc'],
+      ['Creation date (oldest first)', 'created_at_asc'],
+      ['Industry (a-z)', 'industry_name_asc'],
+      ['Category (a-z)', 'category_name_asc']
+    ]
+  end
+
+  def decorated_created_at
+    created_at.to_date.to_s(:long)
+  end
+
 end
