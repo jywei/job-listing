399ddef1402f4f76d8d969129958deae546d419c
Shane <shane.zhou@sarbeer.com>
2016-08-11 21:45:55 +0800

    modify registration controller

--- www/config/routes.rb
+++ www/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   namespace :admin do
     root 'home#index'
     # get 'categories' => 'categories#index'
