0fb7cdb24581f73192326e2f2b3db43406a107d3
Shane <shane.zhou@sarbeer.com>
2016-08-11 13:07:32 +0800

    adjust company backend

--- www/app/assets/javascripts/admin/company.js
+++ www/app/assets/javascripts/admin/company.js
@@ -4,22 +4,25 @@ $.ajax({
   type: "GET",
   dataType: "json",
   success: function(data){
-    alert('success')
     ff = data
     for(var i = 0; i < data.companies.length; i++){
-      $('<tr>' +
-          '<td>' + data.companies[i].name + "</td>" +
-          '<td>' + data.companies[i].tagline + "</td>" +
-          '<td>' + data.companies[i].phone + "</td>" +
-          '<td>' + data.companies[i].email + '</td>' +
-          '<td>' + data.companies[i].about + '</td>' +
-          '<td>' + data.companies[i].android + '</td>' +
-          '<td>' + data.companies[i].facebook + '</td>' +
-          '<td>' + data.companies[i].idea + '</td>' +
-          '<td>' + data.companies[i]. + '</td>' +
-          '<td>' + data.companies[i]. + '</td>' +
-          '<td>' + data.companies[i]. + '</td>' +
-          '<td>' + data.companies[i]. + '</td>' +
+      $('<tr>'
+          + '<td>' + data.companies[i].name + "</td>"
+          + '<td>' + data.companies[i].tagline + "</td>"
+          + '<td>' + data.companies[i].phone + "</td>"
+          + '<td>' + data.companies[i].email + '</td>'
+          + '<td>' + data.companies[i].about + '</td>'
+          + '<td>' + data.companies[i].story + '</td>'
+          + '<td>' + data.companies[i].welfare + '</td>'
+          + '<td>' + data.companies[i].demand + '</td>'
+          + '<td>' + data.companies[i].website + '</td>'
+          + '<td>' + data.companies[i].ios + '</td>'
+          + '<td>' + data.companies[i].android + '</td>'
+          + '<td>' + data.companies[i].facebook + '</td>'
+          + '<td>' + data.companies[i].twitter + '</td>'
+          + '<td>' + data.companies[i].idea + '</td>'
+          + '<td>' + data.companies[i].jobs_count + '</td>'
+        + '</td>'
       ).appendTo($('tbody'))
     }
   },
