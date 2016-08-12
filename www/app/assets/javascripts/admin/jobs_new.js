0c2b1cadcf064c864c9a4f8f1f0152816e9a7255
Shane <shane.zhou@sarbeer.com>
2016-08-12 12:35:38 +0800

    fix new js

--- www/app/assets/javascripts/admin/jobs_new.js
+++ www/app/assets/javascripts/admin/jobs_new.js
@@ -6,12 +6,10 @@ $(document).ready( function() {
 
   function checkCountry(){
     if ($('#job_country_id').val() == '150') {
-      $('#job_location_id').show();
-      $('#job_location_id').siblings($('label')).show();
+      $('.job_location').show();
     }
     else {
-      $('#job_location_id').hide();
-      $('#job_location_id').siblings($('label')).hide();
+      $('.job_location').hide();
     }
   }
 })
